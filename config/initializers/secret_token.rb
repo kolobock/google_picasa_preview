9ffab98fb8a425ef98eb1947ed7292ced68498c9
Andrii Rudenko <kolobock@gmail.com>
2012-11-27 20:58:44 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GooglePicasaPreview::Application.config.secret_token = '8f58bead4a6156b7912c12dbdfda72a0bfbfdc3f840932a3ec9b5c91da492d8d86ca827018d1360e128e5f7b38b82a5d9f1ea193a0971617248b454d1e2d0204'
